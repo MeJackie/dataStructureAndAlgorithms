b854c004b4453e60913befb5b6cca2fc734464fd
jianjian3 <jianjian3@staff.sina.com.cn>
2020-10-21 15:15:42 +0800

    完成单链表及其操作

--- Linklist.php
+++ Linklist.php
@@ -3,58 +3,126 @@
 
 namespace DataStructureAlgorithms;
 
-use DataStructureAlgorithms\Node;
+
+class Node
+{
+    public $val;
+    public $next;
+
+    public function __construct($val = null, $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
 
 class Linklist
 {
     public $head;
-    public $size;    //链表大小
-    public $count;   //当前链表节点数
 
-    public function __construct($size)
+    public function __construct($item = null)
     {
-        $this->head = new Node();
-        $this->count = 0;
-        $this->size = $size;
+        $this->head = new Node($item);
     }
 
-    public function addLast($value){
-        $this->add($this->count, $value);
+    //插入节点
+    public function insert($item, $new) {
+        $newNode = new Node($new);  
+        
+        $current = $this->find($item);
+        $new->next = $current->next;
+        $current->next = $new;
+
+        return $this;
     }
 
-    public function add($index, $value){
-        if($index > $this->size){
-            throw new \Exception("链表已满");
+    // 插入到链尾
+    public function insetLast($item){
+        $current = $this->head;
+        while($current->next != null) {
+            $current = $current->next;
         }
 
-        $prev = $this->head;
-        for($i = 0; $i < $index; $i++){
-            $prev = $prev->next;
-        }
+        $current->next = new Node($item);
 
-        $prev->next = new Node($value, $prev->next);
-
-        $this->count++;
+        return $this;
     }
 
-    public function get($index){
-        if($index > $this->size-1){
-            throw new \Exception("超出链表范围");
+    // 查找节点
+    public function find($item) {
+        $current = $this->head;
+        if($current == null) {
+            throw new \Exception("empty list");
         }
 
-        $prev = $this->head->next;
-        for ($i = 0; $i < $this->size; $i++){
-            if($index == $i){
-                return $prev->val;
-            }
-            $prev = $prev->next;
+        while ($current->val != $item) {
+            $current = $current->next;
         }
+
+        if($current == null) {
+            throw new \Exception("no ".$item." node");
+        }
+
+        return $current;
+    }
+
+    // 删除节点
+    public function remove($item) {
+        $previous = $this->findPrevious($item);
+        $previous->next = $previous->next->next;
+        return true;
+    }
+
+    // 获取节点前一个节点
+    public function findPrevious($item) {
+        $current = $this->head;
+
+        while($current->next != null && $current->next->val != $item){
+            $current = $current->next;
+        }
+
+        if($current == null) {
+            throw new \Exception("no ".$item." node");
+        }
+
+        return $current;
+    }
+
+    // 更新节点
+    public function update($old, $new){
+        $current = $this->find($old);
+        $current->value = $new;
+    }
+
+
+    // 打印节点
+    public function display(){
+        $current = $this->head;
+        if ($current == null) {
+            throw new \Exception("empty list");
+        }
+
+        while($current->next != null){
+            echo $current->val."->";
+            $current = $current->next;
+        }
+
+        echo $current->val;
+        echo "\n";
     }
 }
 
 //include_once 'vendor/autoload.php';
 //
-//$linklist = new Linklist(100);
+$l = new Linklist();
+$l->insetLast(1)->insetLast(2)->insetLast(3);
+$l->display();
+$l->remove(2);
+$l->display();
+$l->remove(1);
+$l->display();
+
+
 //$linklist->addLast(2);
 //$linklist->addLast(3);
 //$linklist->addLast(4);
